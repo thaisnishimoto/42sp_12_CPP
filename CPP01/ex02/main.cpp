6f43e99d7bb7fd72a36215ee6cab8eb121e2bd65
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-08-03 00:25:35 -0300

    Added ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 19:09:11 by tmina-ni          #+#    #+#             */
+/*   Updated: 2024/08/03 00:15:06 by tmina-ni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "The memory address of the string variable: " << &string << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "The value of the string variable: " << string << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
+}
