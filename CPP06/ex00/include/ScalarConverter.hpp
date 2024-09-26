6aaa3ef6a27a4133a7212e2a3f591f6c8fa39503
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-09-26 14:59:23 -0300

    Fix - remove virtual from destructor

--- CPP06/ex00/include/ScalarConverter.hpp
+++ CPP06/ex00/include/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 12:45:12 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/09/23 16:22:09 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/09/26 14:57:35 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 class	ScalarConverter {
 
 public:
-	virtual ~ScalarConverter();
+	~ScalarConverter();
 	static void	converter(const std::string& literal);
 
 private:
