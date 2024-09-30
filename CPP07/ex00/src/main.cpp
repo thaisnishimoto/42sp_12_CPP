3906ebd75df628ff4ea6c072fbac1f694dd525d6
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-09-30 12:07:34 -0300

    Added additional tests

--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 10:34:40 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/09/30 11:34:49 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/09/30 12:06:39 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include "Colors.hpp"
 #include <iostream>
 
 int	main(void)
@@ -34,13 +35,22 @@ int	main(void)
 	{
 	std::cout << std::endl;
 	std::cout << YELLOW << "Additional tests:" << RESET << std::endl;
-	std::cout << YELLOW << "1. Same value" << RESET << std::endl;
-	int	a = 42;
-	int	b = 42;
+	std::cout << YELLOW << "1. Same float value" << RESET << std::endl;
+	float	a = 4.2f;
+	float	b = 4.2f;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
 	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << YELLOW << "2. Double values" << RESET << std::endl;
+	double	c = -42.5;
+	double	d = 42.5;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
 	}
 	return 0;
 }
