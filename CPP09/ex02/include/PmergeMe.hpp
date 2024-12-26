3ddfadbb22fc2b192fb93b7497897686d4be99b5
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-12-26 01:37:12 -0300

    Added recursive merge sort of pairs

--- CPP09/ex02/include/PmergeMe.hpp
+++ CPP09/ex02/include/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 10:21:27 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/12/18 10:21:30 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/12/25 23:09:20 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include <iostream>
 #include <vector>
-#include <list>
+#include <deque>
 #include <set>
 #include <algorithm>
 #include <sstream>
@@ -68,18 +68,69 @@ void printContainer(const Container& sequence)
     std::cout << std::endl;
 }
 
+template <typename PairContainer>
+void printPairs(const PairContainer& pairs)
+{
+    typename PairContainer::const_iterator it = pairs.begin();
+    while (it < pairs.end())
+    {
+        std::cout << "(" << it->first << ", " << it->second << ") ";
+        ++it;
+    }
+    std::cout << std::endl;
+}
+
+template <typename PairContainer>
+void mergeSortPairs(PairContainer& pairs)
+{
+	//base case
+	if (pairs.size() == 1)
+          return;
+
+    //Divide pairs in halves
+    PairContainer left;
+    PairContainer right;
+    for (size_t i = 0; i < pairs.size() / 2; ++i)
+    	left.push_back(pairs[i]);
+    for (size_t i = pairs.size() / 2; i < pairs.size(); ++i)
+    	right.push_back(pairs[i]);
+    mergeSortPairs(left);
+    mergeSortPairs(right);
+
+	//merge sorted halves back, in order
+    size_t i = 0;
+    size_t l = 0;
+    size_t r = 0;
+    while (l < left.size() && r < right.size())
+    {
+    	if (left[l].first < right[r].first)
+        	pairs[i++] = left[l++];
+        else
+        	pairs[i++] = right[r++];
+    }
+    while (l < left.size())
+        pairs[i++] = left[l++];
+    while (r < right.size())
+        pairs[i++] = right[r++];
+    return;
+}
+
 template <typename T, typename PairContainer>
 void mergeInsertionSort(T& container)
 {
+//    //base case
+//    if (container.size() == 1)
+//    	return ;
+//
     //step1: separate number that cannot be paired
-  	int oddOut = -1;
+	int oddOut = -1;
     if (container.size() % 2 != 0)
     {
     	oddOut = container.back();
     	container.pop_back();
 	}
 
-    //step2: divide elements in sorted pairs
+    //step2: divide elements in descending sorted pairs
     PairContainer pairs;
     for (size_t i = 0; i < container.size(); i += 2)
     {
@@ -88,6 +139,28 @@ void mergeInsertionSort(T& container)
         else
     		pairs.push_back(std::make_pair(container[i + 1], container[i]));
     }
+
+    //Step 3: Recursively sort the pairs by larger elements
+    printPairs(pairs);
+    mergeSortPairs<PairContainer>(pairs);
+    printPairs(pairs);
+
+//    //Step 3: Recursively sort the larger elements (from each pair), creating a sorted sequence
+//    T larger;
+//    for (size_t i = 0; i < pairs.size(); ++i)
+//    	larger.push_back(pairs[i].first);
+//    T sorted = mergeInsertionSort(larger);
+//
+//    //Step 4: Create the pend container that holds the second elements of the pairs
+//    // in the same order as the sorted larger elements
+//    for (size_t i = 0; i < sorted.size(); ++i)
+//    {
+//    	if n;
+//}
+
+//    }
+
+    //Step 5: Insert the smallest element of the first pair at the start
 
     if (oddOut != -1)
     	std::cout << "even sequence" << std::endl;
